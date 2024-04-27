7a90811d06bad2c27b117555501ee21b992b9d97
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2024-04-27 08:13:30 -0600

    room database added to di graph

--- app/src/main/java/com/example/obanks/di/AppModule.kt
+++ app/src/main/java/com/example/obanks/di/AppModule.kt
@@ -1,5 +1,9 @@
 package com.example.obanks.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.obanks.data.datasources.local.BankEntityDao
+import com.example.obanks.data.datasources.local.OBanksDatabase
 import com.example.obanks.data.datasources.remote.OBanksService
 import com.example.obanks.data.repositories.BanksRepositoryImpl
 import com.example.obanks.domain.repositories.BanksRepository
@@ -7,6 +11,7 @@ import com.example.obanks.domain.use_cases.GetBanksUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -29,11 +34,26 @@ object AppModule {
     fun providesOBanksService(retrofit: Retrofit): OBanksService =
         retrofit.create(OBanksService::class.java)
 
+    @Singleton
+    @Provides
+    fun providesOBanksDatabase(@ApplicationContext appContext: Context): OBanksDatabase = Room.databaseBuilder(
+        context = appContext,
+        klass = OBanksDatabase::class.java,
+        name = "o_banks_database"
+    ).build()
 
     @Singleton
     @Provides
-    fun providesBanksRepository(oBanksService: OBanksService): BanksRepository =
-        BanksRepositoryImpl(oBanksService)
+    fun providesBankEntityDao(oBanksDatabase: OBanksDatabase): BankEntityDao =
+        oBanksDatabase.bankEntityDao()
+
+    @Singleton
+    @Provides
+    fun providesBanksRepository(oBanksService: OBanksService, bankEntityDao: BankEntityDao): BanksRepository =
+        BanksRepositoryImpl(
+            banksService = oBanksService,
+            bankEntityDao = bankEntityDao
+        )
 
     @Singleton
     @Provides
