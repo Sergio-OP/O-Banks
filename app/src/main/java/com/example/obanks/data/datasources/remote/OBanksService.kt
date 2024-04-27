9b8f73855474fd3ac3e4d73c5898d46a44135bf2
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2024-04-26 19:46:57 -0600

    OBanksService interface created

---
+++ app/src/main/java/com/example/obanks/data/datasources/remote/OBanksService.kt
@@ -0,0 +1,8 @@
+package com.example.obanks.data.datasources.remote
+
+import retrofit2.http.GET
+
+interface OBanksService {
+    @GET("/challenge/banks")
+    suspend fun fetchBanks(): OBanksResponse
+}
